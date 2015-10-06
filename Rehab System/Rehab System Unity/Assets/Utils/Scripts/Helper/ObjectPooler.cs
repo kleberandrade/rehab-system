ff2c69b6927c87c4a7f080b56290a8182963be1a
Kleber de Oliveira Andrade <pdjkleber@gmail.com>
2015-10-06 12:06:26 -0300

    Classes auxiliares

---
+++ Rehab System/Rehab System Unity/Assets/Utils/Scripts/Helper/ObjectPooler.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectPooler : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_ObjectPrefab;
+
+    [SerializeField]
+    private int m_PooledAmount = 3;
+
+    [SerializeField]
+    private bool m_WillGrow = true;
+
+    private List<GameObject> m_PooledObjects;
+
+    void Start()
+    {
+        m_PooledObjects = new List<GameObject>();
+        for (int i = 0; i < m_PooledAmount; i++)
+            CreateNewObject();
+    }
+
+    private GameObject CreateNewObject()
+    {
+        GameObject go = (GameObject)Instantiate(m_ObjectPrefab);
+        go.SetActive(false);
+        m_PooledObjects.Add(go);
+        return go;
+    }
+
+    public GameObject NextObject()
+    {
+        foreach (GameObject go in m_PooledObjects)
+            if (!go.activeInHierarchy)
+                return go;
+
+        if (m_WillGrow)
+            return CreateNewObject();
+
+        return null;
+    }
+}
