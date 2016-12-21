35a10c9dd3ea4404ec372c7dd48c8b7bc08038d0
Bitiquinho <consoni_2519@hotmail.com>
2016-12-21 01:34:49 -0200

    Update velocity instead of position in slave controller

--- Rehab System/Rehab System IMT/DoublePingPong/Assets/Scripts/BatController.cs
+++ Rehab System/Rehab System IMT/DoublePingPong/Assets/Scripts/BatController.cs
@@ -10,20 +10,6 @@ using System.Text;
 [ RequireComponent( typeof(BoxCollider) ) ]
 public class BatController : Controller 
 {
-    /*void FixedUpdate()
-    {
-        // Get remotely controlled object position (z) and set it locally (x)
-		float remoteInput = gameConnection.GetRemoteValue( (byte) elementID, NetworkAxis.X, NetworkValue.POSITION );
-
-        float remotePosition = remoteInput * rangeLimits.x;
-
-        //File.AppendAllText( textFile, Time.realtimeSinceStartup.ToString() + "\t" + remotePosition.ToString() + Environment.NewLine );
-
-		//Debug.Log( "Bat " + elementID.ToString() + " moving to " + ( transform.right * Mathf.Clamp( remotePosition, -rangeLimits.x, rangeLimits.x ) ).ToString() );
-
-		body.MovePosition( transform.right * Mathf.Clamp( remotePosition, -rangeLimits.x, rangeLimits.x ) + initialPosition );
-    }*/
-
 	void FixedUpdate()
 	{
 		Vector3 masterPosition = new Vector3( GameManager.GetConnection().GetRemoteValue( elementID, NetworkAxis.X, NetworkValue.POSITION ),
@@ -31,8 +17,6 @@ public class BatController : Controller
 
 		Vector3 masterVelocity = new Vector3( GameManager.GetConnection().GetRemoteValue( elementID, NetworkAxis.X, NetworkValue.VELOCITY ),
 											  0.0f, GameManager.GetConnection().GetRemoteValue( elementID, NetworkAxis.Z, NetworkValue.VELOCITY ) );
-
-		//Debug.Log( string.Format( "Bat {0} target: position {1} - velocity {2}", elementID, masterPosition, masterVelocity ) );
 
 		body.MovePosition( masterPosition );
 		body.velocity = masterVelocity;
@@ -42,9 +26,6 @@ public class BatController : Controller
 		GameManager.GetConnection().SetLocalValue( elementID, NetworkAxis.Z, NetworkValue.POSITION, body.position.z );
 		GameManager.GetConnection().SetLocalValue( elementID, NetworkAxis.X, NetworkValue.VELOCITY, body.velocity.x );
 		GameManager.GetConnection().SetLocalValue( elementID, NetworkAxis.Z, NetworkValue.VELOCITY, body.velocity.z );
-
-		//File.AppendAllText( logFileName, Time.realtimeSinceStartup.ToString() + "\t" 
-		//	+ ball.position.x.ToString() + "\t" + ball.position.z.ToString() + System.Environment.NewLine );
 	}
 }
 
--- Rehab System/Rehab System IMT/DoublePingPong/Assets/Scripts/SlaveController.cs
+++ Rehab System/Rehab System IMT/DoublePingPong/Assets/Scripts/SlaveController.cs
@@ -18,20 +18,17 @@ public class SlaveController : Controller
 		Vector3 masterVelocity = new Vector3( GameManager.GetConnection().GetRemoteValue( elementID, NetworkAxis.X, NetworkValue.VELOCITY ),
 											  0.0f, GameManager.GetConnection().GetRemoteValue( elementID, NetworkAxis.Z, NetworkValue.VELOCITY ) );
 
-		//Vector3 followingError = masterPosition - body.position;
+		Vector3 followingError = masterPosition - body.position;
 
-		//if( followingError.magnitude < rangeLimits.magnitude / 2 ) masterVelocity += followingError;
-		/*else*/ body.MovePosition( masterPosition );
+		if( followingError.magnitude > rangeLimits.magnitude / 2.0f ) body.MovePosition( masterPosition );
 
 		body.velocity = masterVelocity;
-	}
 
-	/*void OnTriggerEnter( Collider collider )
-	{
-		if( collider.tag == "Vertical" ) body.velocity = new Vector3( -body.velocity.x, 0.0f, body.velocity.z );
-		else if( collider.tag == "Horizontal" ) body.velocity = new Vector3( body.velocity.x, 0.0f, -body.velocity.z );
-		else if( collider.tag == "Tower" ) body.velocity = new Vector3( -body.velocity.x, 0.0f, -body.velocity.z );
-	}*/
+		//GameManager.GetConnection().SetLocalValue( elementID, NetworkAxis.X, NetworkValue.POSITION, body.position.x );
+		//GameManager.GetConnection().SetLocalValue( elementID, NetworkAxis.X, NetworkValue.VELOCITY, body.velocity.x );
+		//GameManager.GetConnection().SetLocalValue( elementID, NetworkAxis.Z, NetworkValue.POSITION, body.position.z );
+		//GameManager.GetConnection().SetLocalValue( elementID, NetworkAxis.Z, NetworkValue.VELOCITY, body.velocity.z );
+	}
 
 	public Vector3 FindImpactPoint( int layerMask )
 	{
