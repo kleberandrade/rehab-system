ee210ce9a13d649bfa1d64f297de0ac7fa842aa2
Bitiquinho <consoni_2519@hotmail.com>
2017-01-02 00:31:56 -0200

    Add initial wave variables controllers.

---
+++ Rehab System/Rehab System IMT/DoublePingPong/Assets/Scripts/WaveMasterController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class WaveMasterController : Controller
+{
+	protected float waveImpedance = 10.0f;
+
+	void FixedUpdate()
+	{
+		Vector3 inputWaveVariable = new Vector3( GameManager.GetConnection().GetRemoteValue( elementID, (int) GameAxis.X, 0 ),
+												 0.0f, GameManager.GetConnection().GetRemoteValue( elementID, (int) GameAxis.Z, 0 ) );
+
+		Vector3 inputWaveIntegral = new Vector3( GameManager.GetConnection().GetRemoteValue( elementID, (int) GameAxis.X, 1 ),
+												 0.0f, GameManager.GetConnection().GetRemoteValue( elementID, (int) GameAxis.Z, 1 ) );
+
+		Vector3 inputForce = waveImpedance * body.velocity - (float) Math.Sqrt( 2.0f * waveImpedance ) * inputWaveVariable;
+		Vector3 inputForceIntegral = waveImpedance * body.position - (float) Math.Sqrt( 2 * waveImpedance ) * inputWaveIntegral;
+
+		body.AddForce( inputForce, ForceMode.Force );
+
+		Vector3 outputWaveVariable = -inputWaveVariable + (float) Math.Sqrt( 2 * waveImpedance ) * body.velocity;
+		Vector3 outputWaveIntegral = -inputWaveIntegral + (float) Math.Sqrt( 2 * waveImpedance ) * body.position;
+
+		GameManager.GetConnection().SetLocalValue( elementID, (int) GameAxis.X, 0, outputWaveVariable.x );
+		GameManager.GetConnection().SetLocalValue( elementID, (int) GameAxis.Z, 0, outputWaveVariable.z );
+		GameManager.GetConnection().SetLocalValue( elementID, (int) GameAxis.X, 1, outputWaveIntegral.x );
+		GameManager.GetConnection().SetLocalValue( elementID, (int) GameAxis.Z, 1, outputWaveIntegral.z );
+	}
+
+	public void OnEnable()
+	{
+		body.position = initialPosition;
+		body.velocity = Vector3.zero;
+	}
+	public void OnDisable()
+	{
+		body.position = initialPosition;
+		body.velocity = Vector3.zero;
+	}
+}
+
---
+++ Rehab System/Rehab System IMT/DoublePingPong/Assets/Scripts/WaveSlaveController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class WaveSlaveController : Controller
+{
+	protected Vector3 outputForce = Vector3.zero;
+	protected Vector3 outputForceIntegral = Vector3.zero;
+
+	protected float waveImpedance = 10.0f;
+
+	void FixedUpdate()
+	{
+		Vector3 inputWaveVariable = new Vector3( GameManager.GetConnection().GetRemoteValue( elementID, (int) GameAxis.X, 0 ),
+											     0.0f, GameManager.GetConnection().GetRemoteValue( elementID, (int) GameAxis.Z, 0 ) );
+
+		Vector3 inputWaveIntegral = new Vector3( GameManager.GetConnection().GetRemoteValue( elementID, (int) GameAxis.X, 1 ),
+												 0.0f, GameManager.GetConnection().GetRemoteValue( elementID, (int) GameAxis.Z, 1 ) );
+
+		Vector3 inputPosition = ( Math.Sqrt( 2 * waveImpedance ) * inputWaveIntegral - outputForceIntegral ) / waveImpedance;
+		Vector3 inputVelocity = ( Math.Sqrt( 2 * waveImpedance ) * inputWaveIntegral - outputForce ) / waveImpedance;
+
+		//body.AddForce( waveImpedance * ( inputPosition - body.position ), ForceMode.Force );
+		body.AddForce( inputVelocity - body.velocity, ForceMode.VelocityChange );
+
+		outputForceIntegral += outputForce;
+		Vector3 outputWaveVariable = inputWaveVariable - (float) Math.Sqrt( 2 / waveImpedance ) * outputForce;
+		Vector3 outputWaveIntegral = inputWaveIntegral - (float) Math.Sqrt( 2 / waveImpedance ) * outputForceIntegral;
+
+		GameManager.GetConnection().SetLocalValue( elementID, (int) GameAxis.X, 0, outputWaveVariable.x );
+		GameManager.GetConnection().SetLocalValue( elementID, (int) GameAxis.Z, 0, outputWaveVariable.z );
+		GameManager.GetConnection().SetLocalValue( elementID, (int) GameAxis.X, 1, outputWaveIntegral.x );
+		GameManager.GetConnection().SetLocalValue( elementID, (int) GameAxis.Z, 1, outputWaveIntegral.z );
+	}
+
+	void OnCollisionStay( Collision collision )
+	{
+		outputForce = collision.impulse / Time.fixedDeltaTime;
+	}
+
+	void OnCollisionExit( Collision collision )
+	{
+		outputForce = Vector3.zero;
+	}
+}
+
